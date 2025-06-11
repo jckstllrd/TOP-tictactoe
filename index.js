5ce283d49a69eaf63534adfbb7d6636a5837dfe3
jckstllrd <jackstallard17@gmail.com>
2025-06-11 21:55:01 +0100

    Add initiateGridCells and createCells functions within displayController for grid setup

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("linked");
+const displayController = (function () {
+  // Set grid variable for use in the object
+  const grid = document.querySelector(".grid");
+
+  function createCell(x, y) {
+    const newCell = document.createElement("div");
+    newCell.classList.toggle("cell");
+    newCell.setAttribute("data-x", x);
+    newCell.setAttribute("data-y", y);
+    newCell.addEventListener("click", (e) => {
+        console.log(e.target);
+        
+    })
+    grid.appendChild(newCell);
+  }
+
+  function initiateGridCells() {
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        createCell(x, y);
+      }
+    }
+  }
+
+  return { initiateGridCells };
+})();
+
+const gameboard = (function () {
+  let board = ([, ,], [, ,], [, ,]);
+})();
+
+const game = (function () {
+  function play() {
+    // initialise board
+    displayController.initiateGridCells();
+  }
+
+  return { play };
+})();
+
+function createPlayer(name) {
+  // Stuff
+  return { name };
+}
+
+game.play();
